9147dfe5389a9d1f7f838afcb10a1fabe99e4263
Joseajierro <jrmar0805@gmail.com>
2025-10-09 12:33:39 +0200

    Pom independiente auth fix

--- auth-service/src/main/java/org/generations/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/org/generations/authservice/AuthServiceApplication.java
@@ -1,12 +1,8 @@
 package org.generations.authservice;
 
-import org.generations.authservice.model.UserApp;
-import org.generations.authservice.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @SpringBootApplication
 public class AuthServiceApplication {
